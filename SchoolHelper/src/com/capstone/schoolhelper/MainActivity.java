2fb68b1318e1059cc557cd2337201ef6918c4f36
Ryan <rmcdole@slu.edu>
2014-02-17 21:41:12 -0600

    Started implementing fragments. Hopefully they will work tomorrow. Fixed class_menu.

--- SchoolHelper/src/com/capstone/schoolhelper/MainActivity.java
+++ SchoolHelper/src/com/capstone/schoolhelper/MainActivity.java
@@ -3,6 +3,11 @@ package com.capstone.schoolhelper;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 
 public class MainActivity extends Activity {
 
@@ -11,6 +16,25 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
     }
-
     
+    Button btnSettings = (Button) findViewById(R.id.btnSettings);
+    Button btnCalendar = (Button) findViewById(R.id.btnCalendar);
+    Button btnClasses = (Button) findViewById(R.id.btnClasses);
+    
+    FragmentManager fragmentManager = getFragmentManager();
+    
+    btnClasses.setOnClickListener(new View.OnClickListener() {
+    	
+    	public void onClick(View arg0){
+    		
+    	    FragmentTransaction transaction = getFragmentManager().beginTransaction();
+    	    ClassesMenuFrag frag = new ClassesMenuFrag();
+    		transaction.replace(R.id.classes_menu, frag);
+    		transaction.addToBackStack(null);
+    		transaction.commit();
+    		
+    	}
+    	
+    });
+
 }
