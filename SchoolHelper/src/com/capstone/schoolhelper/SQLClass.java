726673b6b6f1ef3081a5bbe36eed7727fd0451e0
mschaddel <mschaddel@gmail.com>
2014-03-29 15:21:04 -0500

    Modified files for Databsase. Database not complete due to unkown error.

--- SchoolHelper/src/com/capstone/schoolhelper/SQLClass.java
+++ SchoolHelper/src/com/capstone/schoolhelper/SQLClass.java
@@ -1,5 +1,62 @@
 package com.capstone.schoolhelper;
 
 public class SQLClass {
-
+	String class_name;
+	String professor;
+	String location;
+	String class_time;
+	String documents;
+	
+	public SQLClass(){
+		
+	}
+	
+	public SQLClass(String class_name, String professor, String location, String class_time, String documents){
+		this.class_name = class_name;
+		this.professor = professor;
+		this.location = location;
+		this.class_time = class_time;
+		this.documents = documents;
+		
+	}
+	
+	public String getclassname(){
+		return this.class_name;
+	}
+	
+	public void setclassname(String class_name){
+		this.class_name = class_name;
+	}
+	
+	public String getprofessor(){
+		return this.professor;
+	}
+	
+	public void setprofessor(String professor){
+		this.professor = professor;
+	}
+	
+	public String getlocation(){
+		return this.location;
+	}
+	
+	public void setlocation(String location){
+		this.location = location;
+	}
+	
+	public String getclasstime(){
+		return this.class_time;
+	}
+	
+	public void setmode(String class_time){
+		this.class_time = class_time;
+	}
+	
+	public String getdocuments(){
+		return this.documents;
+	}
+	
+	public void setdocuments(String documents){
+		this.documents = documents;
+	}
 }
--- SchoolHelper/src/com/capstone/schoolhelper/SQLProfile.java
+++ SchoolHelper/src/com/capstone/schoolhelper/SQLProfile.java
@@ -2,4 +2,61 @@ package com.capstone.schoolhelper;
 
 public class SQLProfile {
 
+	String name;
+	String email;
+	String school;
+	String mode;
+	int notifications;
+	
+	public SQLProfile(){
+		
+	}
+	
+	public SQLProfile(String name, String email, String school, String mode, int notifications){
+		this.name = name;
+		this.email = email;
+		this.school = school;
+		this.mode = mode;
+		this.notifications = notifications;
+	}
+	
+	public String getname(){
+		return this.name;
+	}
+	
+	public void setname(String name){
+		this.name = name;
+	}
+	
+	public String getemail(){
+		return this.email;
+	}
+	
+	public void setemail(String email){
+		this.email = email;
+	}
+	
+	public String getschool(){
+		return this.school;
+	}
+	
+	public void setschool(String school){
+		this.school = school;
+	}
+	
+	public String getmode(){
+		return this.mode;
+	}
+	
+	public void setmode(String mode){
+		this.mode = mode;
+	}
+	
+	public int getnotifications(){
+		return this.notifications;
+	}
+	
+	public void setnotifications(int notifications){
+		this.notifications = notifications;
+	}
 }
